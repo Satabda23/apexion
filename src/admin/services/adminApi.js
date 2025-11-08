60c0f3b89968dcfd27c791114bf635fac3947b8a
Saptapda Hazarika <47964609+Satabda23@users.noreply.github.com>
2025-11-08 18:08:53 +0530

    Change API base URL environment variable

--- src/admin/services/adminApi.js
+++ src/admin/services/adminApi.js
@@ -1,6 +1,6 @@
 // src/admin/services/adminApi.js
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+  process.env.REACT_APP_BACKEND_URL || "http://localhost:5000/api";
 
 class AdminApi {
   constructor() {
